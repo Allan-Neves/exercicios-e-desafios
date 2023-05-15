eb1a6b50e3bd8dd60b08947c070b7c767eeaed83
Allan Carlos <allan3017carlos@gmail.com>
2023-05-15 16:49:12 -0300

    Adicionando novos exercícios!

---
+++ javascript/programador-br/ex-004-variaveis/script.js
@@ -0,0 +1,16 @@
+// var a;
+
+// var a = "Placa de vídeo";
+
+// var a;
+// a = "Placa mãe"
+
+// var a;
+// a = "Placa mãe"
+// a = "HD"
+
+var caixa;
+var placa = "Placa de vídeo"
+caixa = placa
+
+console.log(caixa);
