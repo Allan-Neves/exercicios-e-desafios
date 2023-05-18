3dca70c80c517e87ffb4add22eceadfa29aa074d
Allan Carlos <allan3017carlos@gmail.com>
2023-05-18 18:29:46 -0300

    Adicionando exercícios novos.

---
+++ javascript/programador-br/ex-008-operadores-comparativos/script.js
@@ -0,0 +1,48 @@
+// == igual a
+// === valor igual e tipo igual
+// != não é igual
+// !== valor não igual ou tipo não igual
+// > maior que
+// < menor que
+// >= maior que ou igual
+// <= menor que ou igual
+
+var a = 3;
+var b = 3;
+console.log( a == b)
+//=============//
+
+var c = 3;
+var d = "3";
+console.log( c === d)
+//=============//
+
+var e = 5;
+var f = 1;
+console.log( e != f)
+//=============//
+
+var g = 3;
+var h = "3";
+console.log( g !== h)
+//=============//
+
+var i = 4;
+var j = 8;
+console.log( i > j)
+//=============//
+
+var k = 3;
+var l = 9;
+console.log( k < l)
+//=============//
+
+var m = 3;
+var n = 3;
+console.log( m >= n)
+//=============//
+
+var o = 3;
+var p = 2;
+console.log( o <= p)
+//=============//
