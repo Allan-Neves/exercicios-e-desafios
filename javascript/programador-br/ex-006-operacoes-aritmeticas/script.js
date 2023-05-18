9d59a74b61d708c1ed45a57375af56d9b2f574e6
Allan Carlos <allan3017carlos@gmail.com>
2023-05-18 17:20:10 -0300

    Modificando alguns exercícios!

--- javascript/programador-br/ex-006-operacoes-aritmeticas/script.js
+++ javascript/programador-br/ex-006-operacoes-aritmeticas/script.js
@@ -29,27 +29,27 @@ console.log(c);
 // Exercícios
 
 var soma = 25 + 13;
-console.log(soma + ` Adição`);
+console.log(soma + ` | Adição`);
 
 var subtrair = 8 - 32;
-console.log(subtrair + ` Subtração`);
+console.log(subtrair + ` | Subtração`);
 
 var multiplicar = 7 * 5;
-console.log(multiplicar + ` Multiplicação`);
+console.log(multiplicar + ` | Multiplicação`);
 
 var dividir = 45 / 9;
-console.log(dividir + ` Divisão`);
+console.log(dividir + ` | Divisão`);
 
 var cubo = 3 ** 3;
-console.log(cubo + ` Elevado ao cubo`);
+console.log(cubo + ` | Elevado ao cubo`);
 
 var dobro = 15 * 2;
-console.log(dobro + ` Dobro`);
+console.log(dobro + ` | Dobro`);
 
 var subtrair_0 = 80;
 var subtrair_1 = subtrair_0 * 0.2;
 var subtrair_2 = subtrair_0 - subtrair_1;
-console.log(subtrair_2 + ` Subtração 2`);
+console.log(subtrair_2 + ` | Subtração 2`);
 
 var Multiplicação_2 =(2/3) * (9/5);
-console.log(Multiplicação_2 + ` Multiplicação 2`);
+console.log(Multiplicação_2 + ` | Multiplicação 2`);
