05462f359f0def855e1b62bb82ad8c9f6dfaadb0
Allan Carlos <allan3017carlos@gmail.com>
2023-05-19 14:29:49 -0300

    Adicionando e modificando exercícios

--- javascript/programador-br/ex-007-atribuicoes/script.js
+++ javascript/programador-br/ex-007-atribuicoes/script.js
@@ -1,5 +1,5 @@
 // =     x = y     x = y
-// +=    x += y    x = x + y
+// +=    x += y    x = x, y
 // -=    x -= y    x = x - y
 // *=    x *= y    x = x * y
 // /=    x /= y    x = x / y
@@ -18,26 +18,26 @@ console.log(a);
 var ex1 = 5;
 var ex1_b = 3;
 ex1 += ex1_b;
-console.log(ex1 + ` | Ex 1`)
+console.log(ex1, " | Ex 1")
 //=============//
 
 var ex2 = 10;
 ex2 -= 4;
-console.log(ex2 + ` | Ex 2`)
+console.log(ex2, " | Ex 2")
 //=============//
 
 var ex3 = 7;
 var ex3_b = 2;
 ex3 *= ex3_b;
-console.log(ex3 + ` | Ex 3`)
+console.log(ex3, " | Ex 3")
 //=============//
 
 var ex4 = 20;
 ex4 /= 5;
-console.log(ex4 + ` | Ex 4`)
+console.log(ex4, " | Ex 4")
 //=============//
 
 var ex5 = 15;
 var ex5_b = 4;
 ex5 %= ex5_b;
-console.log(ex5 + ` | Ex 5`)
+console.log(ex5, " | Ex 5")
---
+++ javascript/programador-br/ex-009-operadores-logicos/script.js
@@ -0,0 +1,42 @@
+// && e
+// || ou
+// Negativo
+
+var idade = 23;
+
+var maior20 = idade >= 20;
+var menor30 = idade <= 30;
+
+var entre = maior20 && menor30; 
+
+console.log("Idade", idade);
+console.log("Maior que 20", maior20);
+console.log("Menor que 30", menor30);
+console.log("Entre 20 e 30", entre);
+
+console.log("==================")
+
+var idade_2 = 6;
+
+var menor10 = idade_2 <= 10;
+var maior65 = idade_2 >= 65;
+var alimento = false;
+
+var gratuidade = menor10 || maior65 || alimento; 
+
+console.log("Idade", idade_2);
+console.log("Menor que 10", menor10);
+console.log("Maior que 65", maior65);
+console.log("Trouxe alimento?", alimento);
+console.log("Tem direito a gratuidade?", gratuidade);
+
+console.log("==================")
+
+var idade_3 = 230;
+
+var maior40 = idade_3 >= 40;
+var menor40 = !maior40;
+
+console.log("Idade", idade_3);
+console.log("Maior que 40", maior40);
+console.log("Menor que 40", menor40);
