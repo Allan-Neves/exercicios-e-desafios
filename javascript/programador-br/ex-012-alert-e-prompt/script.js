4d369a5c3c79dfab98cec293263e27ff7b472879
Allan Carlos <allan3017carlos@gmail.com>
2023-05-19 15:36:54 -0300

    Adicionando novos exercícios

---
+++ javascript/programador-br/ex-012-alert-e-prompt/script.js
@@ -0,0 +1,17 @@
+// var nome = prompt("Qual o seu nome?");
+// alert("Bom dia, " + nome + "!");
+
+// var numero = prompt("Digite um número!");
+// alert("O número que você digitou foi " + numero);
+
+// var numero1 = parseInt(prompt("Digite um número!"));
+// var dobro = numero1 + numero1;
+
+// alert("O dobro de " + numero1 + " é " + dobro);
+
+var nomeSobrenome = prompt("Qual o seu nome e sobrenome?");
+var idade = parseInt(prompt("Qual a sua idade?"));
+var dobro = idade + idade;
+var corFav = prompt("Qual a sua cor favorita?")
+
+alert("Olá, " + nomeSobrenome + ", você tem " + idade + " anos de idade, uau! Sabia que o dobro da sua idade é igual a " + dobro + ", incrível né? Eu também fiquei sabendo que sua cor favorita é " + corFav + " essa cor deve ser linda!")
---
+++ javascript/programador-br/ex-013-condicional-if/script.js
@@ -0,0 +1,46 @@
+var idade = 25;
+console.log("Idade:", idade)
+
+if (idade >= 35) {
+    console.log("Você pode comprar conosco!");
+    console.log("Qual o seu pedido?");
+}
+
+else if (idade == 25) {
+    console.log("Você pode comprar conosco! E ainda ganha desconto!");
+    console.log("Qual o seu pedido?");
+}
+
+else if (idade >= 18) {
+    console.log("Você pode comprar conosco! Mas me mostre sua identidade.");
+    console.log("Qual o seu pedido?");
+}
+
+else {
+    console.log("Infelizmente você não pode comprar conosco.");
+    console.log("Volte futuramente");
+}
+
+
+
+// if (idade < 18 || idade > 70) {
+//     console.log("Você pode comprar conosco!");
+//     console.log("Qual o seu pedido?");
+// }
+
+// else {
+//     console.log("Infelizmente você não pode comprar conosco.");
+//     console.log("Volte futuramente");
+// }
+
+// ==================================== \\
+
+// if (idade >= 18 && idade <= 70) {
+//     console.log("Você pode comprar conosco!");
+//     console.log("Qual o seu pedido?");
+// }
+
+// else {
+//     console.log("Infelizmente você não pode comprar conosco.");
+//     console.log("Volte futuramente");
+// }
