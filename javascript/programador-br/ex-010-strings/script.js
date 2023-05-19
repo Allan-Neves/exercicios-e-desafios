4bf1d7f0e64368e25ce2e6a94d4a8100eb9fa608
Allan Carlos <allan3017carlos@gmail.com>
2023-05-19 14:56:57 -0300

    Adicionando novos exercícios

---
+++ javascript/programador-br/ex-010-strings/script.js
@@ -0,0 +1,6 @@
+
+var numero = 12;
+var str = "Valor 'texto entre aspas simples' qualquer." + " Mais texto " + numero;
+// str += " Outro texto";
+
+console.log(str);
---
+++ javascript/programador-br/ex-011-strings-em-numeros/script.js
@@ -0,0 +1,12 @@
+
+var a = "3";
+var b = "6";
+var c = parseInt(a) + parseInt(b);
+
+console.log(c);
+
+var d = "3.9";
+var e = "6";
+var f = parseFloat(d) + parseInt(e);
+
+console.log(f);
