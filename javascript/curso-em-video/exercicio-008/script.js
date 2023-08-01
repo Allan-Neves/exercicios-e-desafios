b13c2bf6e7213f8a20c8ed47960db275fff8bc75
Allan Carlos <allan3017carlos@gmail.com>
2023-08-01 17:42:24 -0300

    Atualizando os exercícios de JS

--- javascript/curso-em-video/exercicio-008/script.js
+++ javascript/curso-em-video/exercicio-008/script.js
@@ -1,9 +1,9 @@
 function somar() {
     var tn1 = window.document.getElementById('txtn1');    
-    var tn2 = window.document.querySelector('input#txtn2');
-    var resultado = window.document.getElementById('resultado');
+    var tn2 = window.document.getElementById('txtn2');
     var n1 = Number(tn1.value);    
     var n2 = Number(tn2.value);
+    var resultado = window.document.getElementById('resultado');
     var soma = n1 + n2;
 
     resultado.innerHTML = `A soma entre <strong>${n1}</strong> e <strong>${n2}</strong> é igual a <strong>${soma}</strong>`;
