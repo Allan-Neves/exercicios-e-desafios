73ad9729ffc5224f8c1ed90c5f2572a5b2559228
Allan Carlos <allan3017carlos@gmail.com>
2023-08-08 17:36:46 -0300

    Adicionando novos exercícios de JS

---
+++ javascript/curso-em-video/exercicio-013-pt2/script.js
@@ -0,0 +1,20 @@
+var contador = 1;
+
+do {
+    console.log(`Passo ${contador}`);
+    contador++;
+} while (contador <= 10)
+ 
+
+/* console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`);
+console.log(`Tudo bem?`); */
