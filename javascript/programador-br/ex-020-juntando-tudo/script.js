f2aa32b9e893da552e65bca2619d89a3acecd1d5
Allan Carlos <allan3017carlos@gmail.com>
2023-08-17 14:24:16 -0300

    Modificando método como o exercício é mostrado

--- javascript/programador-br/ex-020-juntando-tudo/script.js
+++ javascript/programador-br/ex-020-juntando-tudo/script.js
@@ -2,14 +2,14 @@
 
 var alunos = ["José", "Maria", "Pedro", "Marta",];
 var notasA = [9.0, 3.0, 7.0, 5,0];
-var notasB = [4.0, 5.0, 10.0, 3.0];
+var notasB = [4.0, 5.0, 10.0, 7.0];
 
 function media(n1, n2) {
     return (n1 + n2) / 2;
 }
 
 function resultado(media) {
-    if (media >= 7) {
+    if (media >= 6.5) {
         return "Aprovado";
     } else {
         return "Reprovado";
@@ -23,5 +23,7 @@ for (var index in alunos) {
     var nota2 = notasB[index];
     var m = media(nota1, nota2);
 
-    console.log(`${aluno} - ${nota1} - ${nota2} - ${m} - ${resultado(m)}`)
+    var text = document.createElement(`p`);
+    document.body.appendChild(text);
+    text.innerHTML = `<strong>${aluno}</strong> | nota A: ${nota1} | nota B: ${nota2} | média: ${m} - <strong>${resultado(m)}</strong>`;
 }
