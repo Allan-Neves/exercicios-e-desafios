ba2a9e1031d2871d9f45c3d561bab9e942e25c69
Allan Carlos <allan3017carlos@gmail.com>
2023-08-24 14:56:12 -0300

    Atualizando a pasta de JS do curso ProgBR

---
+++ javascript/programador-br/intermediario/ex-002-document/script.js
@@ -0,0 +1,18 @@
+var numero1 = 4;
+let numero2 = 5;
+const numero3 = {};
+
+numero2 = 7;
+
+
+
+
+// numero3.nome = "Allan";
+// numero3.push(7);
+
+//numero3 = { nome: "Allan"};
+// numero3 = 7;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
