bf2fc7848062989bc260ccbd862e3ae9308b5fb2
Allan Carlos <allan3017carlos@gmail.com>
2023-09-06 15:25:53 -0300

    Iniciando na criação da lista de compras

--- javascript/programador-br/intermediario/ex-011-local-storage-lista-de-compras/script.js
+++ javascript/programador-br/intermediario/ex-011-local-storage-lista-de-compras/script.js
@@ -1,23 +1,54 @@
-var formulario = document.getElementById("formulario");
-var item = document.getElementById("item");
-var adicionar = document.getElementById("adicionar");
-var remover = document.getElementById("remover");
-var lista = document.getElementById("lista");
+// Função para adicionar uma tarefa à lista
+function adicionarTarefa() {
+    const tarefaInput = document.getElementById("tarefa");
+    const tarefa = tarefaInput.value.trim(); // Remove espaços em branco
 
-function adicionarItem(event) {
-    event.preventDefault();
+    if (tarefa === "") {
+        alert("Por favor, digite uma tarefa.");
+        return;
+    }
 
-    let valor = item.value;
+    // Recupere as tarefas existentes do localStorage ou inicialize uma lista vazia
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
-    if (valor) {
-        let li = document.createElement("li");
+    // Adicione a nova tarefa à lista
+    tarefas.push(tarefa);
 
-        li.textContent = valor;
+    // Atualize o localStorage com a lista atualizada de tarefas
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
 
-        lista.appendChild(li);
+    // Limpe o campo de entrada
+    tarefaInput.value = "";
 
-        item.value = ``;
+    // Atualize a lista de tarefas exibida na página
+    exibirTarefas();
+}
+
+// Função para exibir as tarefas na lista
+function exibirTarefas() {
+    const listaTarefas = document.getElementById("listaTarefas");
+
+    // Limpe a lista atual
+    listaTarefas.innerHTML = "";
+
+    // Recupere as tarefas do localStorage
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+    // Adicione cada tarefa à lista usando um loop for
+    for (let i = 0; i < tarefas.length; i++) {
+        const tarefa = tarefas[i];
+
+        const item = document.createElement("li");
+        item.textContent = `${i + 1}. ${tarefa}`;
+        listaTarefas.appendChild(item);
     }
 }
 
-adicionar.addEventListener("click", adicionarItem)
+// Exiba as tarefas ao carregar a página
+exibirTarefas();
+
+// Adicione um ouvinte de eventos para o botão "Adicionar"
+document.getElementById("adicionar").addEventListener("click", adicionarTarefa);
+
+
+// código feito por IA, ainda estou tentando resolver sozinho
