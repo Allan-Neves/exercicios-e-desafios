40da10be65422cc74d16ec021fea2d3cd97b153d
Allan Neves <allan3017carlos@gmail.com>
2024-08-20 18:58:19 -0300

    Adicionando novos ex

---
+++ javascript/programador-br/basico/ex-019-datas/script.js
@@ -0,0 +1,5 @@
+// var d = new Date();
+// var d = new Date(2005, 6, 22);
+var d = new Date("jul 22 2005 10:34");
+
+console.log(d);
---
+++ javascript/programador-br/basico/ex-020-var-let-const/script.js
@@ -0,0 +1,9 @@
+var numero1 = 4;
+let numero2 = 5;
+const numero3 = [6];
+
+numero3.push(7)
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
