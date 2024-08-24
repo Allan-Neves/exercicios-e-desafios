7ce423d2162133cd8443be37a9235654a9c1b186
Allan Neves <allan3017carlos@gmail.com>
2024-08-23 21:21:04 -0300

    Novos exercícios em Python

---
+++ python/curso-em-video/ex001.py
@@ -0,0 +1,17 @@
+print("====== DESAFIO 1 ======")
+nome = input("Qual é o seu nome? ")
+print("Olá,", nome , "! Prazer em te conhecer.")
+
+
+print("====== DESAFIO 2 ======")
+dia = input("Dia = ")
+mes = input("Mes = ")
+ano = input("Ano = ")
+print("Você nasceu no dia" ,dia, "de" ,mes, "de" ,ano, ". Correto?")
+
+
+print("====== DESAFIO 3 ======")
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+
+print("A soma dos números é:", int(num1) + int(num2))
