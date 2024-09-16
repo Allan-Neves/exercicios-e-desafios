77e43e5ed4573c52a73257a40ba4a720608b8b99
Allan Neves <allan3017carlos@gmail.com>
2024-09-16 18:57:25 -0300

    Adicionando novos exercícios de JS

---
+++ javascript/programador-br/intermediario/ex-001-window/script.js
@@ -0,0 +1,2 @@
+window.console.log("Teste");
+window.alert("Uma mensagem");
---
+++ javascript/programador-br/intermediario/ex-003-modificando-elementos/script.js
@@ -0,0 +1,14 @@
+// console.log(document);
+// console.log(document.body);
+// document.body.innerHTML = "<h1>Esse título foi inserido via JS</h1>" + document.body.innerHTML;
+// console.log(document)
+
+// let p1 = document.getElementById("paragrafo-1");
+// let paragrafos = document.getElementsByClassName("paragrafo");
+
+// console.log(p1);
+// console.log(paragrafos);
+
+let tp = document.getElementsByTagName("p");
+
+console.log(tp);
